d42993308a9a4772f0e4b47fa9f01d100e6b5ce0
Vilius <vilius@vilius.net>
2024-04-12 18:41:20 +0300

    Create initial app layout in html and css

--- habtrack/src/App.vue
+++ habtrack/src/App.vue
@@ -10,23 +10,58 @@ import { RouterLink, RouterView } from 'vue-router'
       <div class="days-container">
         <div class="day">
           <div class="day__date">7</div>
-          <div class="day__week-day">SUN</div>
+          <div class="day__week-day">sun</div>
+        </div>
+        <div class="day">
+          <div class="day__date">6</div>
+          <div class="day__week-day">sat</div>
+        </div>
+        <div class="day">
+          <div class="day__date">5</div>
+          <div class="day__week-day">fri</div>
+        </div>
+        <div class="day">
+          <div class="day__date">4</div>
+          <div class="day__week-day">thu</div>
+        </div>
+        <div class="day">
+          <div class="day__date">3</div>
+          <div class="day__week-day">wed</div>
         </div>
       </div>
       <div class="habbits-container">
         <div class="habbit">
           <div class="habbit__title">
-            <div class="habbit__color"></div>
-            <div class="habbit__name"></div>
+            <div class="habbit__color">o</div>
+            <div class="habbit__name">Make the bed</div>
           </div>
-          <div class="habbit__labels"></div>
+          <div class="habbit__cells">
+            <dir class="habbit__cell">x</dir>
+            <dir class="habbit__cell">x</dir>
+            <dir class="habbit__cell">x</dir>
+            <dir class="habbit__cell">x</dir>
+            <dir class="habbit__cell">x</dir>
+          </div>
+        </div>
+        <div class="habbit">
+          <div class="habbit__title">
+            <div class="habbit__color">o</div>
+            <div class="habbit__name">Brush my teeth</div>
+          </div>
+          <div class="habbit__cells">
+            <dir class="habbit__cell success">v</dir>
+            <dir class="habbit__cell">x</dir>
+            <dir class="habbit__cell">x</dir>
+            <dir class="habbit__cell">x</dir>
+            <dir class="habbit__cell">x</dir>
+          </div>
         </div>
         <div class="bottom-container">
           <div class="new-habbit">
-            <div class="new-habbit__btn"></div>
-            <div class="new-habbit__name"></div>
+            <div class="new-habbit__btn">+</div>
+            <div class="new-habbit__name">New habbit</div>
           </div>
-          <div class="message-panel"></div>
+          <div class="message-panel">Add new habbit by pressing +</div>
         </div>
       </div>
     </div>
@@ -36,64 +71,107 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  /* margin: 5px; */
 }
-
 .logo {
   font-size: 3ch;
 }
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 1rem;
+.days-container {
+  display: flex;
+  /* grid-template-columns: repeat(5, 1fr); */
+  /* border: 1px solid gray; */
+  /* flex-basis: auto; */
+  align-self: flex-end;
+}
+.day {
+  width: 40px;
+  height: 40px;
+  /* border: 1px solid gray; */
+}
+.day__date {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.day__week-day {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.habbits-container {
+  display: flex;
+  flex-direction: column;
+  /* border: 1px solid gray; */
+  row-gap: 1px;
+}
+.habbit {
+  display: flex;
+  align-items: center;
+  flex: 1;
+  justify-content: space-between;
+  border: 1px solid gray;
+}
+.habbit__title {
+  display: flex;
+  /* border: 1px solid blue; */
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.habbit__color {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 20px;
+  flex-shrink: 0;
+}
+.habbit__name {
+  display: flex;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.habbit__cells {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  /* border: 1px solid green; */
+}
+.habbit__cell {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 40px;
+  height: 40px;
+  /* border: 1px solid yellow; */
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.success {
+  color: greenyellow;
 }
 
-nav a:first-of-type {
-  border: 0;
+.bottom-container {
+  display: flex;
+  /* border: 1px solid yellow; */
+  height: 40px;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.new-habbit {
+  display: flex;
+}
+.new-habbit__btn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 20px;
+  flex-shrink: 0;
+  /* border: 1px solid yellow; */
+}
+.new-habbit__name {
+  display: flex;
+  align-items: center;
+  width: 130px;
+}
+.message-panel {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
